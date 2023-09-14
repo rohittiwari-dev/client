930f2d7c0c5d1fa97dcd85de1d3bbcf9b2171f29
Rohit Tiwari <137717184+rohittiwari-dev@users.noreply.github.com>
2023-09-14 18:52:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			// Proxy requests to '/api' to your API server
-			"/api": {
-				target: "https://schedular-backend.onrender.com/api/v1", // Replace with the URL of your API server
-				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api/, ""),
-			},
-		},
-		watch: {
-			usePolling: true,
-		},
-	},
 });
