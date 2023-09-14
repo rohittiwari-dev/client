caba0ded493b64b93a0576d64e9ea02432b85740
Rohit Tiwari <137717184+rohittiwari-dev@users.noreply.github.com>
2023-09-14 12:39:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), pluginRewriteAll()],
+	server: {
+		proxy: {
+			'/api/v1': {
+				target: "https://schedular-backend.onrender.com/api/v1" ,
+				changeOrigin: true,
+			},
+		},
+	},
 });
